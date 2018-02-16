b20a0ab0f1ab8b2ca734bff15c12825aaf08ad8d
Loic DAVID <loic.david@altran.com>
2018-02-16 16:15:52 +0100

    Added missing AssemblyInformationalVersion in AssemblyInfo.cs

--- csharp-metar-decoder/Properties/AssemblyInfo.cs
+++ csharp-metar-decoder/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("csharp-metar-decoder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open-source CSharp METAR Decoder library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("SAFRANCassiopee")]
 [assembly: AssemblyProduct("csharp-metar-decoder")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
 [assembly: AssemblyTrademark("")]
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
